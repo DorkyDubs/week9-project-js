68a4016bf371ea2f7bb331f4339654c965e34354
Dorkydubs <rsgacre@gmail.com>
2024-07-21 22:55:12 +0100

    hope to fic vercel

---
+++ src/app/user/[userId]/undefined/page.js
@@ -0,0 +1,8 @@
+export default function StillsGoAwayPlease() {
+  return (
+    <h4 className=" text-2xl">
+      {" "}
+      You are lost, please head home and look elsewhere
+    </h4>
+  );
+}
---
+++ src/app/user/undefined/pages.js
@@ -0,0 +1,8 @@
+export default function GoAwayPlease() {
+  return (
+    <h4 className=" text-2xl">
+      {" "}
+      You are lost, please head home and look elsewhere
+    </h4>
+  );
+}
